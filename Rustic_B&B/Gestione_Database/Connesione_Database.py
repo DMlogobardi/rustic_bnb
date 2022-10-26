15c8451255ca65145e228ba185605c54f92149c5
DMlogobardi <116758219+DMlogobardi@users.noreply.github.com>
2022-10-26 22:00:58 +0200

    Update Connesione_Database.py

--- Rustic_B&B/Gestione_Database/Connesione_Database.py
+++ Rustic_B&B/Gestione_Database/Connesione_Database.py
@@ -1,11 +1,21 @@
+from select import select
+from sqlite3 import Cursor, connect
 import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
+  port = "3306",
   user="root",
   password="",
-  port = 3306,
   database = "bnb"
 )
 
-print(mydb)
+cursor = mydb.cursor()
+
+#quary for test
+cursor.execute("SELECT * FROM utente")
+
+result = cursor.fetchall()
+
+for x in result:
+  print(x)
